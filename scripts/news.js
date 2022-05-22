3a9e7d97ea12b2a5e4aaf3289bf3a297739f2b7f
fan-of-ghost <dmitriiloschilov2003@gmail.com>
2022-05-22 19:49:39 +0300

    initial

---
+++ scripts/news.js
@@ -0,0 +1,42 @@
+
+
+const info = [
+    {
+        text: 'Dumplings are very cool!<br />@floppa',
+        photo: 'url("./images/floppa/1.jpg")'
+    },
+    {
+        text: 'Am I small russian cat? WTF?<br />@chmonya',
+        photo: 'url("./images/chmonya/1.jpg")'
+    },
+    {
+        text: 'WHO AM I?<br />@popcat',
+        photo: 'url("./images/popcat/1.jpg")'
+    }
+]
+
+
+const rightButton = document.querySelector('.right')
+const leftButton = document.querySelector('.left')
+
+const photo = document.querySelector('.photo')
+const text = document.querySelector('.text')
+
+let index = 0
+
+
+rightButton.onclick = () => {
+    if (++index == info.length)
+        index = 0
+
+    photo.style.backgroundImage = info[index].photo
+    text.innerHTML = info[index].text
+}
+
+leftButton.onclick = () => {
+    if (--index == -1)
+        index = info.length - 1
+
+    photo.style.backgroundImage = info[index].photo
+    text.innerHTML = info[index].text
+}
